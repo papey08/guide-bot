416b8d3983d71882be47d48f10c666d49bad9aa7
papey08 <popov.m4tvei@yandex.ru>
2024-12-08 15:27:58 +0300

    add entities

---
+++ app/entities/place.py
@@ -0,0 +1,6 @@
+class Place:
+    def __init__(self, name, url, location, category):
+        self.name = name
+        self.url = url
+        self.location = location
+        self.category = category
