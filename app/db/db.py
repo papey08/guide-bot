7c276655f9cd41d218a6a3bc4d826132d5df6be8
papey08 <popov.m4tvei@yandex.ru>
2024-12-11 21:20:35 +0300

    update Db class

--- app/db/db.py
+++ app/db/db.py
@@ -1,4 +1,29 @@
+import app.entities as entities
+
 class Db:
     # todo
-    def __init__(self, db_config):
+    def __init__(self, db_host, db_port, db_username, db_password, db_database_name):
+        pass
+
+    # todo
+    def add_user(self, username: str):
+        """
+        Добавляет нового пользователя
+        """
+        pass
+
+    # todo
+    def add_response(self, username: str, place_id: int):
+        """
+        Добавляет место, предложенное пользователю, в таблицу response
+        """
+        pass
+
+    def find_place(self, category: str, location: str):
+        """
+        Находит место с заданной категорией и локацией
+        Если в данной локации нет места с данной категорией, ищем место из соседней локации (app/entities/locations.py)
+        Если и в соседней локации нет места с данной категорией, ищем место из любой другой локации.
+        :returns: entities.Place
+        """
         pass
--- app/entities/place.py
+++ app/entities/place.py
@@ -1,6 +1,5 @@
 class Place:
-    def __init__(self, name, url, location, category):
+    def __init__(self, name, url, location):
         self.name = name
         self.url = url
         self.location = location
-        self.category = category
